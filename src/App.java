eb03beb73c168e4b7eb230f543f51500b0252db2
Jarvis <tangyiqwan@MacBook-Air.local>
2021-07-14 13:08:22 +0800

    some changes

--- src/App.java
+++ src/App.java
@@ -18,7 +18,15 @@ public class App {
         int a = 5, b = 2;
         float result = a / b; // 2.0 because a / b is int
         float result2 = (float) a / b;
+        System.out.println(result);
         System.out.println(result2);
         
+        switch (a) {
+            case 1: b = 1; break;
+            case 5: b = 5; break;
+            default: b = 0;
+
+        }
+        System.out.println("b is " + b);
     }
 }
